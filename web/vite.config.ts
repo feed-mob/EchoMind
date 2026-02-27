3ffb8f62510076f294ef720489643b7236de7ce8
roofeel <roofeel@live.com>
2026-02-27 16:51:10 +0800

    add allowedHosts configuration for server and preview in vite.config.ts

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -5,11 +5,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: ['xo8k8g0wwkg8o8w40wsgs08w.184.73.145.124.sslip.io'],
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
         changeOrigin: true,
       },
     },
+  },
+  preview: {
+    allowedHosts: ['xo8k8g0wwkg8o8w40wsgs08w.184.73.145.124.sslip.io'],
   },
 })
